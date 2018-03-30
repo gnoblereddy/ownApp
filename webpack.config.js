aa66d6831cd404a917ea6968873ebc4dd671426c
Noble4c6 <noble@peoplelinkvc.com>
2018-03-30 16:23:26 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './App.js',
+    output: {
+        path: '/',
+        filename: 'index.js',
+    },
+    devServer: {
+        inline: true,
+        port: 8060
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/, //check for all js files
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+              presets: ['react', 'es2015']
+            }
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' },
+                { loader: 'css-loader' }]
+            },
+        ]
+    }
+}
