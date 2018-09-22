463501d3940182b81e37a04b69bbdc6615c743f6
gnoblereddy <gnoblereddy@gmail.com>
2018-09-22 13:14:15 +0530

    removed consoles in routes.js file

--- server/routes.js
+++ server/routes.js
@@ -6,16 +6,11 @@ var Response = require('./commons/response');
 
 
 router.post('/signup', function (req, res) {
-    // console.log("backend");
     var data = new db(req.body);
     data.save(function(err,data){
      if(err){
-    //    console.log(err);
          Response.errorResponse(err.message,res);
      }else{
-        //  console.log("-----",data);
-        //  console.log("vyibujnkl",res.data)
-        //  Response.successResponse('User registered successfully!',res.data);
          return res.status(200).json({ result: data, message: 'User registered successfully!' , success:1, statusCode:200,status:true});
      } 
     })
@@ -24,23 +19,16 @@ router.post('/signup', function (req, res) {
 router.post('/login', function (req, res, next) {
     var email = req.body.username;
     var password = req.body.password;
-    console.log("----", req.body);
 
     db.findOne({ email: email, password:password },
         function (err, data) {
-            console.log("1");
             if (err) {
-                console.log(err);
                 Response.errorResponse(err.message,res);
             }
             if (!data) {
                 Response.notFoundResponse('Invalid Email Id or Password!',res);
             }
-            else {
-                console.log("2")
-            //    req.session.user = email;
-            //    Response.successResponse('User loggedin successfully!',res,user);
-                
+            else {                
                return res.status(200).json({ result: data, message: 'User loggedin successfully!' , success:1, statusCode:200,status:true});
             }
         }
